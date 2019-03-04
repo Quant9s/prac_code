b9707b594733448fdacc9b9913cd8a773595569a
jisung kwon <kquant9s@gmail.com>
2019-03-05 00:04:01 +0900

    logging_my.log

--- logging_prac.py
+++ logging_prac.py
@@ -4,7 +4,10 @@ if __name__ == '__main__':
     mylogger = logging.getLogger("my")
     mylogger.setLevel(logging.INFO)
 
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
     stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
     mylogger.addHandler(stream_handler)
 
     file_handler = logging.FileHandler("my.log")
