866f32a4761cf0c7c011973b24858f8e2d6bd3c7
jisung kwon <kquant9s@gmail.com>
2019-03-04 23:53:14 +0900

    logging

--- logging_prac.py
+++ logging_prac.py
@@ -1,1 +1,13 @@
-a
+import logging
+
+if __name__ == '__main__':
+    mylogger = logging.getLogger("my")
+    mylogger.setLevel(logging.INFO)
+
+    stream_handler = logging.StreamHandler()
+    mylogger.addHandler(stream_handler)
+
+    file_handler = logging.FileHandler("my.log")
+    mylogger.addHandler(file_handler)
+
+    mylogger.info("server start!!")
